49423882f7eb9fa40d38cdd42157605650b508ab
Gabriel_FS13 <falcaoshadowbr@gmail.com>
2022-04-14 12:42:28 -0300

    First Commit

---
+++ rating star, JS/script.js
@@ -0,0 +1,178 @@
+function ratio(form, value){
+  
+    if(form == 1){
+        c = 1
+        while(c <= 10){
+            let img = document.getElementById('img1'+c)
+            img.src = 'Vstar.png'
+            c += 1
+        }
+    
+        if(document.getElementById('img1'+value).src = 'Vstar.png'){
+            count = 1
+            while(count <= value){
+            let img = document.getElementById('img1'+count)
+            img.src = 'Cstar.png'
+            count += 1
+            }
+        }
+    
+        let div = document.getElementById('result')
+        div.innerHTML = '<b>'+ value+'</b> / 10'
+    
+    }
+
+    if(form == 2){
+        c = 1
+        while(c <= 10){
+            let img = document.getElementById('img2'+c)
+            img.src = 'Vstar.png'
+            c += 1
+        }
+    
+        if(document.getElementById('img2'+value).src = 'Vstar.png'){
+            count = 1
+            while(count <= value){
+            let img = document.getElementById('img2'+count)
+            img.src = 'Cstar.png'
+            count += 1
+            }
+        }
+    
+        let div = document.getElementById('result2')
+        div.innerHTML = '<b>'+ value+'</b> / 10'
+    
+    }
+
+    if(form == 3){
+        c = 1
+        while(c <= 10){
+            let img = document.getElementById('img3'+c)
+            img.src = 'Vstar.png'
+            c += 1
+        }
+    
+        if(document.getElementById('img3'+value).src = 'Vstar.png'){
+            count = 1
+            while(count <= value){
+            let img = document.getElementById('img3'+count)
+            img.src = 'Cstar.png'
+            count += 1
+            }
+        }
+    
+        let div = document.getElementById('result3')
+        div.innerHTML = '<b>'+ value+'</b> / 10'
+    
+    }
+
+    if(form == 4){
+        c = 1
+        while(c <= 10){
+            let img = document.getElementById('img4'+c)
+            img.src = 'Vstar.png'
+            c += 1
+        }
+    
+        if(document.getElementById('img4'+value).src = 'Vstar.png'){
+            count = 1
+            while(count <= value){
+            let img = document.getElementById('img4'+count)
+            img.src = 'Cstar.png'
+            count += 1
+            }
+        }
+    
+        let div = document.getElementById('result4')
+        div.innerHTML = '<b>'+ value+'</b> / 10'
+    
+    }
+
+    if(form == 5){
+        c = 1
+        while(c <= 10){
+            let img = document.getElementById('img5'+c)
+            img.src = 'Vstar.png'
+            c += 1
+        }
+    
+        if(document.getElementById('img5'+value).src = 'Vstar.png'){
+            count = 1
+            while(count <= value){
+            let img = document.getElementById('img5'+count)
+            img.src = 'Cstar.png'
+            count += 1
+            }
+        }
+    
+        let div = document.getElementById('result5')
+        div.innerHTML = '<b>'+ value+'</b> / 10'
+    
+    }
+
+    if(form == 6){
+        c = 1
+        while(c <= 10){
+            let img = document.getElementById('img6'+c)
+            img.src = 'Vstar.png'
+            c += 1
+        }
+    
+        if(document.getElementById('img6'+value).src = 'Vstar.png'){
+            count = 1
+            while(count <= value){
+            let img = document.getElementById('img6'+count)
+            img.src = 'Cstar.png'
+            count += 1
+            }
+        }
+    
+        let div = document.getElementById('result6')
+        div.innerHTML = '<b>'+ value+'</b> / 10'
+    
+    }
+
+    if(form == 7){
+        c = 1
+        while(c <= 10){
+            let img = document.getElementById('img7'+c)
+            img.src = 'Vstar.png'
+            c += 1
+        }
+    
+        if(document.getElementById('img7'+value).src = 'Vstar.png'){
+            count = 1
+            while(count <= value){
+            let img = document.getElementById('img7'+count)
+            img.src = 'Cstar.png'
+            count += 1
+            }
+        }
+    
+        let div = document.getElementById('result7')
+        div.innerHTML = '<b>'+ value+'</b> / 10'
+    
+    }
+    if(form == 8){
+        c = 1
+        while(c <= 10){
+            let img = document.getElementById('img8'+c)
+            img.src = 'Vstar.png'
+            c += 1
+        }
+    
+        if(document.getElementById('img8'+value).src = 'Vstar.png'){
+            count = 1
+            while(count <= value){
+            let img = document.getElementById('img8'+count)
+            img.src = 'Cstar.png'
+            count += 1
+            }
+        }
+    
+        let div = document.getElementById('result8')
+        div.innerHTML = '<b>'+ value+'</b> / 10'
+    
+    }
+
+}   
